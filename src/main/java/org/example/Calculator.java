51213bfecbf54dc0f201969d143f258230731f8a
Vladislav-Kholetskiy <vladislav.kholetsky@gmail.com>
2024-12-23 22:19:38 +0300

    Update Calculator

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return a + b;
     }
     public int subtract(int a, int b) {
-        return a - b;
+        return b - a;
     }
     public int multiply(int a, int b) {
         return a * b;
